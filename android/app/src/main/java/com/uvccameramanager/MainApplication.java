0f7016adc45025ba523e8c2360806f9043b9fdd9
Li Zheng <flyskywhy@gmail.com>
2018-06-04 21:16:34 +0800

    new step 9: `react-native link react-native-image-crop-picker`

--- android/app/src/main/java/com/uvccameramanager/MainApplication.java
+++ android/app/src/main/java/com/uvccameramanager/MainApplication.java
@@ -3,6 +3,7 @@ package com.uvccameramanager;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecomponent.barcode.RCTCapturePackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PickerPackage(),
             new RNCameraPackage(),
             new RCTCapturePackage()
       );
