fb75958b9b918549ae2380c297f5321efdb4da43
Li Zheng <flyskywhy@gmail.com>
2018-07-27 06:44:25 +0800

    feat(react-native-uvc-camera): pass compile again by fixing `Could not resolve com.facebook.react:react-native:+`

--- post-install.sh
+++ post-install.sh
@@ -20,5 +20,11 @@ sed -i -e "s/PlatformHelpers'/PlatformHelpers.native'/" node_modules/react-navig
 echo patching webdriver-manager
 sed -i -e "s/outputDir]);/outputDir], { stdio: 'inherit' });/" node_modules/webdriver-manager/built/lib/cmds/update.js
 
+echo patching react-native-image-crop-picker
+sed -i -e "s/'com.facebook.react:react-native:+'/('com.facebook.react:react-native:0.51.0') { force = true }/" node_modules/react-native-image-crop-picker/android/build.gradle
+
+echo patching react-native-smart-barcode
+sed -i -e "s/'com.facebook.react:react-native:+'/('com.facebook.react:react-native:0.51.0') { force = true }/" node_modules/react-native-smart-barcode/android/build.gradle
+
 # echo patching video.js
 # sed -i -e "s/(browser.ANDROID_VERSION/(!(\/UCBrowser\/i.test(window.navigator.userAgent)) \&\& browser.ANDROID_VERSION/" node_modules/video.js/es5/tech/html5.js
