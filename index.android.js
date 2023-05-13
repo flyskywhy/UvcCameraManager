741257b36b5e02b1b4a1874cb36c80ccf46c9e7e
Li Zheng <flyskywhy@gmail.com>
2023-05-13 18:39:26 +0800

    describe the reason why use index.android.js not index.native.js

--- index.android.js
+++ index.android.js
@@ -1,3 +1,7 @@
+// 之前发现需要 index.android.js 而非 index.native.js 才能让
+// react-native-code-push@5.2.0 和 bugsnag-react-native@2.5.1
+// 正常工作
+
 import {AppRegistry} from 'react-native';
 import App from './app/index';
 import {name as appName} from './app.json';
